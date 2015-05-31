9cc586bc3afe8ed4704030f87dd40f2302cf532f
Jeff Plourde <jeff@jeffplourde.com>
2015-05-31 08:15:26 -0400

    more flexible regex for signal definition

--- wfdb.js
+++ wfdb.js
@@ -1,6 +1,7 @@
 var record = process.argv[2] || (console.log("Specify a record") || process.exit(-1));
 
 var fs = require('fs');
+var http = require('http');
 
 fs.readFile(record+'.hea', {encoding: 'ascii'}, function(err, data) {
 	if(err) {
@@ -9,13 +10,14 @@ fs.readFile(record+'.hea', {encoding: 'ascii'}, function(err, data) {
 	} else {
 		// Comment lines
 		data = data.replace(/\s*#.*/g, "");
+
 		// Blank lines
 		data = data.replace(/^\s*$/gm, "");
 
 		var lines = data.split("\n");
 
 		var re = /^(\S+)(?:\/(\d+))?\s+(\d+)\s+([0-9e\-.]+)?(?:\/([0-9e\-.]+))?(?:\(([\d-.]+)\))?(?:\s+(\d+))?(?:\s+(\S+))?(?:\s+(\S+))?/;
-		console.log(lines[0]);
+
 		var header = re.exec(lines[0]);
 		var info = {
 			name: header[1],
@@ -30,13 +32,15 @@ fs.readFile(record+'.hea', {encoding: 'ascii'}, function(err, data) {
 		};
 		console.log(info);
 		
+		
 		var signals = [];
 		for(var i = 0; i < info.number_of_signals; i++) {
-			var fmt = /^(\S+)\s+(\S+)(?:x(\d+))?(?:\:(\d+))?(?:\+(\d+))?\s+([\d-.]+)(?:\(([\d-.]+)\))?(?:\/(\S+))?\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.*)$/g;
+			var fmt = /^(\S+)\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?(?:[Ee]\d+)?)(?:x([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\:([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\(([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?)\))?(?:\/(\S+))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+([+-]?\d*(?:\.\d+)?(?:[Ee]\d+)?))?(?:\s+(.+))?\r?$/g;
+
 			var arr = fmt.exec(lines[i+1]);
 			signals.push(
 				{
-					file_name: arr[1],
+					file_name: arr[1],	
 					format: arr[2],
 					samples_per_frame: arr[3] || 1,
 					skew: arr[4] || 0,
@@ -52,37 +56,58 @@ fs.readFile(record+'.hea', {encoding: 'ascii'}, function(err, data) {
 					description: arr[14] || ""
 				}
 			);
+			if(signals[signals.length-1].format != 212) {
+				var discarded = signals.pop();
+				console.log("Cannot decode format " + discarded.format + " for " + discarded.description);
+			}
 		}
-		console.log(signals);
 
-		process.exit(0);
+		if(signals.length == 0) {
+			return;
+		}
+
+		var  top_line = ['sequence','elapsed'];
+		signals.forEach(function(value, index, array) {
+			top_line.push(value.description);
+		});
+		console.log(top_line.join("\t"));
+
+
+
 		fs.readFile(record+'.dat', function(err, data) {
 			if(err) {
 				console.log(err);
 				process.exit(-1);
 			} else {
-				var time_interval = 1000.0 / 360.0;
+				var time_interval = 1000.0 / info.sampling_frequency;
 
 				var elapsed_ms = 0;
 
-				for(var i = 0; i < data.length; i+=12) {
+				for(var i = 0; i < data.length; i+=12) { // TODO not always 12 bytes per frame
 					var frame = [];
 					for(var j = 0; j < signals.length; j++) {
-						var adc;
-						if(0 == (j%2)) {
-							adc = ((data.readUInt8(i+j/2*3+1)&0x0F)<<8) + data.readUInt8(i+j/2*3);
-						} else {
-							adc = ((data.readUInt8(i+(j-1)/2*3+1)&0xF0)<<4) + data.readUInt8(i+(j-1)/2*3+2);
-						}
-						var sign = 0 != (0x800&adc) ? -1 : 1;
-						if(sign < 0) {
-							adc = ~adc + 1;
-						}
+						var value;
+						switch(signals[i].format) {
+							case 212:
+								var adc;
+								if(0 == (j%2)) {
+									adc = ((data.readUInt8(i+j/2*3+1)&0x0F)<<8) + data.readUInt8(i+j/2*3);
+								} else {
+									adc = ((data.readUInt8(i+(j-1)/2*3+1)&0xF0)<<4) + data.readUInt8(i+(j-1)/2*3+2);
+								}
+								var sign = 0 != (0x800&adc) ? -1 : 1;
+								if(sign < 0) {
+									adc = ~adc + 1;
+								}
 
-						adc &= 0x7FF;
-						adc *= sign;
+								adc &= 0x7FF;
+								adc *= sign;
 
-						var value = (adc - signals[j].baseline) / signals[j].adc_gain;
+								value = (adc - signals[j].baseline) / signals[j].adc_gain;
+								break;
+							default:
+								break;
+						}
 
 						frame.push(Math.round(value*1000)/1000);						
 
