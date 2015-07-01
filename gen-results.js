2b27f73dae4ef248ba47f4019b220131bb01fc11
Jeff Plourde <jeff@jeffplourde.com>
2015-07-01 16:33:01 -0400

    gen-results.js with no arguments will re-generate results for all the unit tests.

--- gen-results.js
+++ gen-results.js
@@ -1,9 +1,11 @@
 "use strict";
 
+
+var fs = require('fs');
 /**
  * Generate JSON encoded results from test samples to be used for unit testing
  */
-var record = process.argv[2] || (console.log("Specify a record") || process.exit(-1));
+var record = process.argv[2];
 
 var WFDB = require('./wfdb.js');
 
@@ -11,19 +13,38 @@ var locator = new WFDB.FileLocator('test/data/');
 
 var wfdb = new WFDB(locator);
 
-var results = {rows: []};
-wfdb.readHeaderAndData(record, function(res) {
-    res.on('header', function(header) {
-        results['header'] = header;
-    }).on('data', function(sequence, data) {
-        results.rows.push(data);
-    }).on('error', function(err) {
-        console.log(err);
-    }).on('end', function() {
-        console.log(JSON.stringify(results, null, 4));
-    });
-}); 
-
-
-
-
+if(record) {
+    var results = {rows: []};
+    wfdb.readHeaderAndData(record, function(res) {
+        res.on('header', function(header) {
+            results['header'] = header;
+        }).on('data', function(sequence, data) {
+            results.rows.push(data);
+        }).on('error', function(err) {
+            console.log(err);
+        }).on('end', function() {
+            console.log(JSON.stringify(results, null, 4));
+        });
+    }); 
+} else {
+    var test_records = JSON.parse(fs.readFileSync('test_records.json', 'utf8')).test_records;
+    for(var i = 0; i < test_records.length; i++) {
+        record = test_records[i];
+        
+        wfdb.readHeaderAndData(record, function(res) {
+            var results = {rows: []};
+            var record_ = record;
+            res.on('header', function(header) {
+                results['header'] = header;
+            }).on('data', function(sequence, data) {
+                results.rows.push(data);
+            }).on('error', function(err) {
+                console.log(err);
+            }).on('end', function() {
+                var filename = 'test/'+record_+'.json';
+                fs.writeFileSync(filename, JSON.stringify(results, null, 4));
+                console.log(filename);
+            });
+        }); 
+    }
+}
