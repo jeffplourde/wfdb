ac08a17b17f624b0991a5435e30c1405098dbdcd
Jeff Plourde <jeff@jeffplourde.com>
2015-07-01 16:32:30 -0400

    Test using both readHeaderAndData as well as with readFrames (for 10 frames)

--- test/test-data.js
+++ test/test-data.js
@@ -11,14 +11,14 @@ var wfdb = new WFDB(locator);
 
 module.exports = exports = function(name, record) {
 
-    describe(name, function() {
+    describe(name+" readHeaderAndData", function() {
         var results = {rows: []};
         var expected = JSON.parse(fs.readFileSync('test/'+record+'.json', 'utf8'));
-        it('decode '+name+' records correctly', function(done) {
+        it('decode '+name+' records correctly with readHeaderAndData', function(done) {
             wfdb.readHeaderAndData(record, function(res) {
                 res.on('header', function(header) {
                     results['header'] = header;
-                }).on('data', function(sequence, data) {
+                }).on('data', function(sample_number, data) {
                     results.rows.push(data);
                 }).on('error', function(err) {
                     done(err);
@@ -27,6 +27,28 @@ module.exports = exports = function(name, record) {
                     assert.deepEqual(JSON.parse(JSON.stringify(results)), expected);
                     done();
                 });
+            });
+        });
+    }); 
+
+    describe(name+" readFrames", function() {
+        var results = {rows: []};
+        var expected = JSON.parse(fs.readFileSync('test/'+record+'.json', 'utf8'));
+        it('decode '+name+' records correctly with readFrames', function(done) {
+            wfdb.readHeader(record, function(res) {
+                res.on('data', function(header) {
+                    results['header'] = header;
+                    wfdb.readFrames(header, 0, 10, function(res1) {
+                        res1.on('data', function(sample_number, data) {
+                            results.rows.push(data);
+                        }).on('error', function(err) { done(err); })
+                        .on('end', function() {
+                            // parse/stringify eliminates undefined properties that wouldn't have made it into the JSON
+                            assert.deepEqual(JSON.parse(JSON.stringify(results)), expected);
+                            done();
+                        });
+                    });
+                }).on('error', function(err) { done(err); });
             });
         });
     }); 
